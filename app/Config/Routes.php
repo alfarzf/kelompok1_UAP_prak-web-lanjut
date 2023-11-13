31eefac6dcf9c7aa93a4884064e7a4a48b6caeda
Alfa Rizki <alfarizkif.29@gmail.com>
2023-11-13 23:08:16 +0700

    nambah CRUD

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -12,6 +12,19 @@ $routes->get('/login', 'Home::login');
 
 // Pengurus
 $routes->get('/pengurus', [PengurusController::class ,'index']);
+$routes->get('/pengurus/blok', [PengurusController::class ,'blok']);
+$routes->get('/pengurus/blok/create', [PengurusController::class ,'blok_create']);
+$routes->get('/pengurus/blok/(:any)/edit', [PengurusController::class ,'blok_edit']);
+$routes->post('/pengurus/blok/store', [PengurusController::class ,'blok_store']);
+$routes->put('/pengurus/blok/(:any)', [PengurusController::class ,'blok_update']);
+$routes->delete('/pengurus/blok/(:any)', [PengurusController::class ,'blok_destroy']);
 
-// warga
+$routes->get('/pengurus/informasi', [PengurusController::class ,'informasi']);
+$routes->get('/pengurus/informasi/create', [PengurusController::class ,'informasi_create']);
+$routes->get('/pengurus/informasi/(:any)/edit', [PengurusController::class ,'informasi_edit']);
+$routes->post('/pengurus/informasi/store', [PengurusController::class ,'informasi_store']);
+$routes->put('/pengurus/informasi/(:any)', [PengurusController::class ,'informasi_update']);
+$routes->delete('/pengurus/informasi/(:any)', [PengurusController::class ,'informasi_destroy']);
+
+//Warga
 $routes->get('/warga', [WargaController::class ,'index']);
--- app/Controllers/PengurusController.php
+++ app/Controllers/PengurusController.php
@@ -2,11 +2,66 @@
 
 namespace App\Controllers;
 use App\Controllers\BaseController;
+use App\Models\BlokModel;
+use App\Models\InformasiModel;
 
 class PengurusController extends BaseController
 {
+    public $blokModel;
+    public $informasiModel;
+
+    public function __construct(){
+        $this->blokModel = new BlokModel();
+        $this->informasiModel = new InformasiModel();
+    }
     public function index()
     {
         return view('pengurus/home');
     }
+#Blok
+    public function blok()
+    {
+        $data = [
+            // 'title' => 'List User',
+            'bloks' => $this->blokModel->getBlok()
+        ];
+        return view('pengurus/blok', $data);
+    }
+
+    public function blok_create(){
+        return view('pengurus/create_blok');
+    }
+
+    public function blok_store(){
+        $data=[
+            'nama_blok' => $this->request->getVar('nama_blok')
+        ];
+        $this->blokModel->saveBlok($data);
+        return redirect()->to('/pengurus/blok');
+    }
+
+    public function blok_destroy($id){
+        $this->blokModel->deleteBlok($id);
+        return redirect()->to('/pengurus/blok');
+    }
+
+    public function blok_edit($id){
+        $data=[
+            'blok' => $this->blokModel->getBlok($id)
+        ];
+        return view('pengurus/edit_blok', $data);
+    }
+
+    public function blok_update($id){
+        $data = [
+            'nama_blok' => $this->request->getVar('nama_blok')
+        ];
+        $result = $this->blokModel->updateBlok($data, $id);
+        if(!$result){
+            return redirect()->back()->withInput()->with('error', 'Gagal Menyimpan Data');
+        }
+        return redirect()->to('/pengurus/blok');
+    }
+
+    
 }
